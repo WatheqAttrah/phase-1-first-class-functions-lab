bbe599efce6f44875e37b3b1c00262a04865d8d2
Watheq Attrah <novaatrah@gmail.com>
2023-03-22 17:39:35 -0700

    Great Lab

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 // Code your solution in this file!
+
+
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo' ];
+
+const returnFirstTwoDrivers = function(drivers) {
+    return drivers.slice(0,2);
+}
+returnFirstTwoDrivers(drivers);
+
+const returnLastTwoDrivers = function(drivers) {
+    return drivers.slice(-2);
+}
+returnLastTwoDrivers(drivers);
+
+const selectingDrivers = [returnFirstTwoDrivers,returnLastTwoDrivers];
+
+function createFareMultiplier() {
+    return function fareMultiplier(fareRide) {
+        return fareRide * fareRide;
+    }
+}
+createFareMultiplier(4);
+
+
+
+const fareDoubler = function fareDoubler(createFareMultiplier) {
+        return createFareMultiplier * 2;
+}
+fareDoubler(10);
+
+const fareTripler = function fareTripler(createFareMultiplier){
+    return createFareMultiplier * 3;
+}
+fareTripler(36);
+
+//const selectDifferentDrivers = (drivers, returnFirstTwoDrivers) => returnFirstTwoDrivers(drivers);
+//const selectDifferentDrivers2 = (drivers, returnLastTwoDrivers) => returnLastTwoDrivers(drivers);
+
+
+function selectDifferentDrivers(drivers, returnFirstTwoDrivers) {
+    return returnFirstTwoDrivers(drivers);
+}
+selectDifferentDrivers(drivers,returnFirstTwoDrivers);
